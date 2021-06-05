4fdd41b7236dd839e79b64f297bd59a5bd0dbe7c
merci <1978427972@qq.com>
2021-06-05 16:25:14 +0800

    init7

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
+/** jsdoc
+ * @type { import('@vue/cli-service').ProjectOptions }
+ */
+
 module.exports = {
   outputDir: 'dist',
-  publicPath: process.env.NODE_ENV === 'production' ? '/mercizzy.github.io/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-auto/' : '/'
 }
